c501ca10e7795b81f58851bce4a329229e67caac
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-06 21:42:03 -0500

    updated addEquipment

--- src/controllers/equipment.ts
+++ src/controllers/equipment.ts
@@ -5,28 +5,21 @@ import { errorHandler } from '../middleware';
 class EquipmentController {
 
     public addEquipment = async(req: Request, res: Response, next: any) => {
-        const { name, type, description, userId, muscles} = req.body;
-
-        const user_id = req.body.userId;
-
-        if (!user_id){
-            throw "User Needed"
-        }
-
-        // check if user exists
-        const user = await prisma.user.findUnique({
-            where: { id: user_id }
-        });
-
-        if (!user){
-            throw "User not found"
-        }
-
-        if (!req.body.name || !req.body.type) {
-            next('Name and type are required')
-        }   
-
         try {
+            const { name, type, description, userId, muscles} = req.body;
+
+            if (!name && !type && !userId && !muscles ){
+                return errorHandler('Equipment name, type and muscles are required', req, res, next)
+            }
+
+            const user = await prisma.user.findUnique({
+                where: { id: userId }
+            });
+
+            if (!user){
+                return errorHandler('User not found', req, res, next)
+            }
+
             const newEquipment = await prisma.equipment.create({
                 data: {
                     name,
@@ -34,17 +27,20 @@ class EquipmentController {
                     description,
                     userId,
                     targetMuscles: {
-                        create: muscles.map((muscle: string) => ({muscle: muscle}))
+                        create: muscles.map(
+                            (muscle: string) => ({muscle: muscle})
+                        )
                     }
                 },
                 include: {
                     targetMuscles: true
                 }
             });
+            
             res.status(201).json({
                 success: true,
                 data: newEquipment,
-                message: 'Post created successfully',
+                message: 'Equipment created successfully',
             });
         } catch (error) {
             errorHandler(error, req, res, next)

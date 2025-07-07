14e15e518d5aecd0bd1b8ca5ce3d64daf8d801cd
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-06 21:01:34 -0500

    added route for updating a user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,6 +5,7 @@ const router = Router();
 router.post('/register', UsersController.registerUser);
 router.post('/login', UsersController.loginUser);
 router.get('/:id', UsersController.getUserById);
+router.put('/:id', UsersController.updateUserDetails);
 
 
 export default router;

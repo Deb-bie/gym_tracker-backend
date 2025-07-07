9ae7055b877e72ac0a88568b77c2fe1bb2869698
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-06 22:05:32 -0500

    controller for finding all equipments

--- src/controllers/equipment.ts
+++ src/controllers/equipment.ts
@@ -20,6 +20,15 @@ class EquipmentController {
                 return errorHandler('User not found', req, res, next)
             }
 
+            // check if email already exixts
+            const existingequipment = await prisma.equipment.findUnique({
+                where: { name: name }
+            });
+
+            if (existingequipment){
+                return errorHandler("Equipment Name is already being used", req, res, next)
+            }
+
             const newEquipment = await prisma.equipment.create({
                 data: {
                     name,
@@ -80,6 +89,37 @@ class EquipmentController {
                 success: true,
                 data: equipment,
                 message: 'Equipment retrieved successfully',
+            });
+        } catch (error) {
+            errorHandler(error, req, res, next)
+            console.log("err: ", error)
+        }
+    }
+
+    public getAllEquipments = async(req: Request, res: Response, next: any) => {
+        try {
+            const { id } = req.params;
+            const equipmentId = parseInt(id);
+
+            if (isNaN(equipmentId)) {
+                return errorHandler("Invalid user Id", req, res, next);
+            }
+
+            const equipments = await prisma.equipment.findMany({
+                include: {
+                    targetMuscles: {
+                        select: {
+                            id: true,
+                            muscle: true
+                        },
+                    },
+                },
+            });
+            
+            res.status(201).json({
+                success: true,
+                data: equipments,
+                message: 'Equipments retrieved successfully',
             });
         } catch (error) {
             errorHandler(error, req, res, next)

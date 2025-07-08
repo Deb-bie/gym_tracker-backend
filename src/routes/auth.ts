ba49947674198531033ef6f7071a67cc48efcfc9
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-07 23:37:35 -0500

    added rooute for getting the current user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -4,6 +4,7 @@ const router = Router();
 
 router.post('/register', UsersController.registerUser);
 router.post('/login', UsersController.loginUser);
+router.get('/me', UsersController.getCurrentUser)
 router.get('/:id', UsersController.getUserById);
 router.put('/:id', UsersController.updateUserDetails);
 router.delete('/:id', UsersController.deleteUser);

ff44bd4dc0c29895f9f43f3e05b25bc737213ab9
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-16 23:21:49 -0500

    added recent routes

--- src/routes/workoutsession.ts
+++ src/routes/workoutsession.ts
@@ -3,6 +3,7 @@ import WorkoutSessionController from '../controllers/workoutSession';
 const router = Router();
 
 router.post('/', WorkoutSessionController.createWorkoutSession);
+router.get('/recent', WorkoutSessionController.getRecentWorkouts)
 router.get('/:id', WorkoutSessionController.getWorkoutSessionById);
 router.patch('/:id', WorkoutSessionController.endWorkoutSession)
 

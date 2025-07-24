d6cc98388665e63f21e315e58c8815c729cbf057
Deb-bie <asamoahjaeldeborah@gmail.com>
2025-07-23 21:05:41 -0500

    active workout session

--- src/routes/workoutsession.ts
+++ src/routes/workoutsession.ts
@@ -4,6 +4,7 @@ const router = Router();
 
 router.post('/', WorkoutSessionController.createWorkoutSession);
 router.get('/recent', WorkoutSessionController.getRecentWorkouts)
+router.get('/active', WorkoutSessionController.getActiveWorkouts)
 router.get('/:id', WorkoutSessionController.getWorkoutSessionById);
 router.patch('/:id', WorkoutSessionController.endWorkoutSession)
 
